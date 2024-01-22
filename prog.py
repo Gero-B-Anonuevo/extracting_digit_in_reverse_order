fb0fbb2ee0de970c887c0eb7f41dc19b12ed8cf5
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 19:18:09 +0800

    printing of resulting number not working

--- prog.py
+++ prog.py
@@ -4,8 +4,12 @@ from tkinter import simpledialog
 number = tk.simpledialog.askinteger("Hello!", "Input your number: ")
 number_str = str(number)
 digit_count = len(number_str)
+resulting_var = ""
 
 while digit_count >= 1:
     digit_position = digit_count - 1
-    print(number_str[digit_position])
+    specific_digit = number_str[digit_position]
     digit_count = digit_count - 1
+    resulting_var + " " + specific_digit
+
+print(resulting_var)
