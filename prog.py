be526396489dbdbd24a99b151f06a513531a2fb1
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 19:11:53 +0800

    reversing done

---
+++ prog.py
@@ -0,0 +1,16 @@
+import tkinter as tk 
+from tkinter import simpledialog
+
+number = tk.simpledialog.askinteger("Hello!", "Input your number: ")
+#reverse_number_string = str(number)[::-1]
+number_str = str(number)
+digit_count = len(number_str)
+resulting_var = ""
+
+while digit_count >= 1:
+    digit_position = digit_count - 1
+    specific_digit = number_str[digit_position]
+    digit_count = digit_count - 1
+    resulting_var = resulting_var + " " + specific_digit
+
+print(resulting_var)
